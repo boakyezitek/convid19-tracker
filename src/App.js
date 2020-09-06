c8afb0d323de78454d5c757a42a45044dd6dc5e1
Boakye51 <53356674+Boakye51@users.noreply.github.com>
2020-09-06 16:27:28 +0000

    Fetch country list from api and select box created containing  country list.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Card,
+  CardContent,
+  Select
+} from '@material-ui/core'
 import './App.css';
 
+
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  useEffect(() => {
+     const getCountriesData = async () => {
+       await fetch("https://disease.sh/v3/covid-19/countries")
+       .then((response) => response.json())
+       .then((data) => {
+         const countries = data.map((country) => (
+              {
+                name:country.country,
+                value:country.countryInfo.iso2
+              }
+         ));
+         setCountries(countries);
+       })
+     }
+
+     getCountriesData();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    console.log("Yooooo", countryCode);
+    setCountry(countryCode);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__header">
+      <h1>Covid 19 tracker</h1>
+
+      <FormControl className="app__dropdown">
+            <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+           {
+              countries.map((country, index) => (
+
+                  <MenuItem value={country.value} key={index}>{country.name}</MenuItem>
+
+              ))
+           }
+
+      </Select>
+      </FormControl>
+      </div>
+
+
+      {/* header */}
+      {/* tittle + select input dropdown field */}
+
+      {/* info Boxs */}
+     <div className="app__stats">
+
+     </div>
+       {/* table */}
+      {/* Graph */}
+       {/* Map */}
     </div>
   );
 }
